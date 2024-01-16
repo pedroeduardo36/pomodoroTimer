72fc079c20d001003c576911e52913d1c4115d44
pedroeduardo36 <pedrovilarinho.gpd@gmail.com>
2024-01-16 16:16:00 -0300

    Configurando temas

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -17,8 +17,14 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 50px;
+  border-radius: 4px;
+  border: 0px;
+  margin: 8px;
 
-  ${(props) => {
+  background-color: ${props => props.theme.primary};
+  color: ${props => props.theme.white};
+
+  /* ${(props) => {
     return css`background-color: ${buttonVariants[props.variant]}`;
-  }}
+  }} */
 `;
